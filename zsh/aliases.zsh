a0177768d942393de9ced310f32d757799503185
Matheus Klauberg <mfklauberg@gmail.com>
2018-03-28 19:14:12 -0300

    add zsh aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+## ALIASES ##
+
+## weather
+alias wwork='curl -4 http://wttr.in/Blumenau'
+alias whome='curl -4 http://wttr.in/Pomerode'
+
+## zsh
+alias ezsh='vi ~/.zshrc'
+alias rzsh='source ~/.zshrc'
+
+##
+alias cls='clear'
