b3a27ceb7b722bf5b38edff4acaafc8409a8a1e5
Matheus Klauberg <mfklauberg@gmail.com>
2018-11-14 09:47:13 -0200

    feat: fn to reset network location

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,4 +1,8 @@
-# change network location based on available wifi network.
+# change network location based on wifi
+# depends on symbolic link to
+# ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+
+## change network location
 function wifiloc() {
   WORK_WIFI="PHBNURDS"
   NETWORK_LOCATION="Automatic"
@@ -6,6 +10,12 @@ function wifiloc() {
   if airport -s | grep -q $WORK_WIFI; then
     NETWORK_LOCATION="Philips"
   fi
+
+  networksetup -switchtolocation $NETWORK_LOCATION &> /dev/null && tok "Network location switched to $NETWORK_LOCATION."
+}
+
+function wifires() {
+  NETWORK_LOCATION="Automatic"
 
   networksetup -switchtolocation $NETWORK_LOCATION &> /dev/null && tok "Network location switched to $NETWORK_LOCATION."
 }
